046f6afaf5d33e1902ca7dd57c9c044f3aeabc98
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-04-07 01:53:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let ans="";
+for (let num = 1; num < 101; num++) {
+	if (num%3 === 0 && num%5 === 0) {
+		ans += "FizzBuzz\n";
+		
+	}
+	else if(num%3 === 0){
+		ans += "Fizz\n";
+		
+	}
+	else if ( num%5 === 0) {
+		ans += "Buzz\n";
+	
+	}
+	else{
+		ans += num+"\n";
+	}
+}
+alert(ans);  
